c9bf5bdde1f73e7cf3f5f87238f7835f89857741
Nicat <webdevelopernicat@gmail.com>
2024-12-10 19:47:17 +0400

    task_8/data-base connection

---
+++ src/main/java/org/example/DAO/Ticket.java
@@ -0,0 +1,37 @@
+package org.example.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Ticket {
+    public void saveTicket(int id, int userId, String ticketType, String creationDate) throws SQLException {
+        String sql = "INSERT INTO public.ticket (id, user_id, ticket_type, creation_date) " +
+                "VALUES (?, ?, ?::ticket_type, ?)";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            statement.setInt(2, userId);
+            statement.setString(3, ticketType);
+            statement.setString(4, creationDate);
+            statement.executeUpdate();
+        }
+    }
+
+
+    public void fetchTicketsByUserId(int userId) throws SQLException {
+        String sql = "SELECT * FROM public.ticket WHERE user_id = ?";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println("ID: " + resultSet.getInt("id"));
+                System.out.println("User ID: " + resultSet.getInt("user_id"));
+                System.out.println("Ticket Type: " + resultSet.getString("ticket_type"));
+                System.out.println("Creation Date: " + resultSet.getString("creation_date"));
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/TicketType.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum TicketType {
+    DAY, WEEK, MONTH, YEAR;
+}
