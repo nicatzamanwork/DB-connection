5f0c638c4b8f7b35bce00be9d69ec2293cbe6072
Nicat <webdevelopernicat@gmail.com>
2024-12-11 17:08:29 +0400

    features created

---
+++ src/example/DAO/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package org.example.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/my_ticket_service_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1234";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
