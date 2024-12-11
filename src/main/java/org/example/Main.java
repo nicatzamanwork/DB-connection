2c8ff40fc34f598e8c7db4d4eea3eb9d34802486
Nicat <webdevelopernicat@gmail.com>
2024-12-11 17:00:03 +0400

    reverted

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.example.DAO.Ticket;
+import org.example.DAO.UserTable;
+
+public class Main {
+    public static void main(String[] args) {
+        UserTable userDao = new UserTable();
+        Ticket ticketDao = new Ticket();
+
+        try {
+            userDao.saveUser(3, "John Doe", "2024-12-09");
+
+            userDao.fetchUserById(3);
+
+            ticketDao.saveTicket(3, 3, "DAY", "2024-12-09");
+
+            ticketDao.fetchTicketsByUserId(3);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
